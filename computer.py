f0a39d530b98ec333b82eb1e975c1dde73e8afcd
Seif Wael <seifeldin7666@gmail.com>
2021-12-04 11:16:40 +0200

    Level Limit added, Children are put in list according to the column insertion.

--- computer.py
+++ computer.py
@@ -1,7 +1,10 @@
 import numpy as np
 import math
 import library as lib
+
 COLUMN_INDEX= 0
+SCORE_INDEX=1
+MAX_LEVEL= 4
 
 def get_computer_decision(board_state:np.ndarray) -> np.int8:
     """
@@ -11,6 +14,9 @@ def get_computer_decision(board_state:np.ndarray) -> np.int8:
     return maximize(board_state.copy())[COLUMN_INDEX]
 
 def maximize(state:np.ndarray, level:int, alpha:float = None, beta:float= None) -> tuple:
+
+    if level == MAX_LEVEL:
+        return (-1,compute_objective_function(state)) 
     
     if lib.is_state_complete(state):
         return (-1, get_score(state)) #get_score needs to be identified (What does it return indeed)
@@ -19,16 +25,17 @@ def maximize(state:np.ndarray, level:int, alpha:float = None, beta:float= None) 
     
     state_children= get_children(state)
     
-    for child in state_children:
+    for i in range (len(state_children)):
+        if state_children[i] is None:
+            continue
 
-        (pos, score)= minimize(child, level+1, alpha, beta)
+        score= minimize(state_children[i], level+1, alpha, beta)[SCORE_INDEX]
 
         if score > max_score:
             max_score= score
-            max_pos= pos
+            max_pos= i
         
         if alpha is not None and beta is not None:
-            
             if max_score >= beta:
                 break
             if max_score > alpha:
@@ -38,6 +45,9 @@ def maximize(state:np.ndarray, level:int, alpha:float = None, beta:float= None) 
 
 
 def minimize(state:np.ndarray, level:int, alpha:float = None, beta:float= None) -> tuple:
+
+    if level == MAX_LEVEL:
+        return (-1,compute_objective_function(state)) 
     
     if lib.is_state_complete(state):
         return (-1, get_score(state)) #get_score needs to be identified (What does it return indeed)
@@ -46,13 +56,15 @@ def minimize(state:np.ndarray, level:int, alpha:float = None, beta:float= None) 
     
     state_children= get_children(state)
     
-    for child in state_children:
+    for i in range (len(state_children)):
+        if state_children[i] is None:
+            continue
 
-        (pos, score)= maximize(child, level+1, alpha, beta)
+        score= maximize(state_children[i], level+1, alpha, beta)[SCORE_INDEX]
 
         if score < min_score:
             min_score= score
-            min_pos= pos
+            min_pos= i
         
         if alpha is not None and beta is not None:
             
